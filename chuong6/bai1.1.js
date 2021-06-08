679f33cfae5b7091e1e82d8f416413ee6cf9c25b
tranngocdai <85327489+tranngocdai@users.noreply.github.com>
2021-06-08 16:58:49 +0700

    baithuchanh6

---
+++ chuong6/bai1.1.js
@@ -0,0 +1,6 @@
+function a(){
+    //document.write("Hello VN");
+    //console.log("hello VN");
+    //document.getElementById("bai1.1).innerText= "Hello Vn";
+    document.getElementById("bai1.1").innerHTML = "<h1>Hello VN</h1>";
+}
---
+++ chuong6/bai3.js
@@ -0,0 +1,6 @@
+function a(){
+    //let name = document.getElementById("name").value;
+    //console.log(name);
+    name = prompt("Moi nhap ten");
+    document.getElementById("bai3").innerText = name;
+}
